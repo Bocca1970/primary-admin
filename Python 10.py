adb6d99beb8f2b32f36cc93d9f4424516348d074
Borislav Jovanovic <jborislav1970@gmail.com>
2017-11-27 21:42:49 +0100

    Prvog dana ukupno 11 .py fajlova

---
+++ Python 10.py
@@ -0,0 +1,29 @@
+
+# --------------- Python 10 ---------------
+# ----------- Logicke Operacije -----------
+
+import operator
+ 
+x = True
+y = False
+
+# Binarni operator logicko i (&):
+print ("binarni operator logicko i - x&y je: ", end="")
+print (operator.and_(x, y))
+
+# Binarni operator logicko ili (|):
+print ("binarni operator logicko ili - x|y je: ", end="")
+print (operator.or_(x, y))
+
+# Unarni operator negacije ~x:
+print ("Unarni operator negacije - ~x: ", end="")
+print (operator.not_(x))
+
+''' Operand 1     and      or    Operand 2
+     True        True     True    True
+     True        False    True    False
+     False       False    True    True
+     False       False    False   False
+'''
+
+
---
+++ Python 11.py
@@ -0,0 +1,25 @@
+
+# --------------- Python 11 ---------------
+# -------- Izracunavanje faktorijela ------
+
+print("---------- Izracunaj faktorijel ----------")
+print('\r')
+broj = int(input("Unesi broj: "))
+print('\r')
+
+def fct(broj):
+    blank = ' ' * (2 * broj)
+    print(blank, 'faktorijel', broj)
+    print('\r')
+    if broj == 0:
+        print (blank, 'rezultat je: 1')
+        print('\r')
+        return 1
+    else:
+        nastavak = fct(broj-1)
+        result = broj * nastavak
+        print (blank, 'rezultat je: ', result)
+        print('\r')
+        return result
+
+print ("Faktorijal broja ", broj, "je", fct(broj))
