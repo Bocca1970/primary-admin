38ffbf630761a0401a0bd742a50fc461ba375ca8
Borislav Jovanovic <jborislav1970@gmail.com>
2017-11-27 17:54:10 +0100

    First to files for GitHub

---
+++ Python 1.py
@@ -0,0 +1,15 @@
+
+# ------- Python Numbers --------
+
+print(type(44)) # integer - svaki celi broj, bilo negativan bilo pozitivan, ukljucujuci 0
+# class 'int'
+
+print(type('12.5')) # sve izmedju navodnika, cak i broj, postaje string
+# class 'str'
+
+print(type(12.5)) # float (pun naziv - floating point) - svaki realni broj, bilo pozitivan bilo negativan
+# class 'float'
+
+print(type(2+1j)) # complex - svaki kompleksni broj
+# class 'complex' 
+
---
+++ Python 2.py
@@ -0,0 +1,22 @@
+
+# ------- Python Sequences --------
+
+s = '12asd34KL'
+print(type(s)) # string - konacni uredjeni skup koji se nalazi izmedju navodnika
+# class 'str'
+
+l = [10, 'lista', 23.34]
+print(type(l)) # list - konacni uredjeni skup koji se nalazi unutar uglastih zagrada
+# class 'list' 
+
+t = (12, 'asd', '34KL')
+print(type(t)) # tuple - konacni uredjeni skup, kao i 'list', samo sto je ovaj skup nepromenljiv 
+# class 'tuple'
+
+c = {10, 1, 20, 2, 30, 3, 40, 4, 50, 5}
+print(type(c)) # set - neuredjeni skup jedinstvenih karaktera unutar viticastih zagrada
+# class 'set' 
+
+d = {'Ime:' : 'Bocca', 'Key' : 'Value'}
+print(type(d)) # dict - recnik vrsi mapiranje kljuca (key) u neku odredjenu vrednost (value)
+# class 'dict'
