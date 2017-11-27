cdb65be9730c4a4306c8477b4756d7da199b0e4d
Borislav Jovanovic <jborislav1970@gmail.com>
2017-11-27 18:17:10 +0100

    We continue with the GitHub repo

---
+++ Python 3.py
@@ -0,0 +1,26 @@
+
+# ------- Python Conversion I--------
+
+f = int(43.54)
+print(type(f)) # Konverzija realnog broja u celi broj
+# class 'int'
+
+i = float(122)
+print(type(i)) # Konverzija celog broja u realni broj
+# class 'float'
+
+n = str(44)
+print(type(n)) # Konverzija celog broja u string
+# class 'str'
+
+s = int(88)
+print(type(s)) # Konverzija stringa u celi broj
+# class 'int'
+
+m = str(22.123)
+print(type(m)) # Konverzija realnog broja u string
+# class 'str'
+
+t = float('124.36')
+print(type(t)) # Konverzija stringa u realni broj
+# class 'float'
