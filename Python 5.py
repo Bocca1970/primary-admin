7cc988de5cb8226f5b24e6056175814998377494
Borislav Jovanovic <jborislav1970@gmail.com>
2017-11-27 19:08:31 +0100

    Let us continue with No.5

---
+++ Python 5.py
@@ -0,0 +1,17 @@
+
+# # ------- Python Arithmetics --------
+
+n = int(input('Unesi vrednost n: '))
+m = int(input('Unesi vrednost m: '))
+
+# Sabiranje:
+print(n, '+', m, '=', n + m)
+
+# Oduzimanje:
+print(n, '-', m, '=', n - m)
+
+# Mnozenje:
+print(n, '*', m, '=', n * m)
+
+# Deljenje:
+print(n, '/', m, '=', n / m)
