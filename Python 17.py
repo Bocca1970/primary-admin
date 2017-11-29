c77109c50f91dca16d73262c9251b2bc8f234e2b
Borislav Jovanovic <jborislav1970@gmail.com>
2017-11-29 19:06:07 +0100

    Izracunavanje povrsine kruga

---
+++ Python 17.py
@@ -0,0 +1,14 @@
+
+# --------------- Python 16 ---------------
+# ------------- Povrsina kruga ------------
+
+import math
+
+poluprecnik = float(input("Unesi duzinu poluprecnika: "))
+
+def povrsina(poluprecnik):
+    p = math.pi * poluprecnik**2
+    return p
+print('\r')
+
+print ("Povrsina kruga je: ", povrsina(poluprecnik))
