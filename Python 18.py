7aa7e7df40e66a238186ea186b90f9502394fe16
Borislav Jovanovic <jborislav1970@gmail.com>
2017-11-29 19:09:39 +0100

    Pitagorina teorema

---
+++ Python 18.py
@@ -0,0 +1,19 @@
+
+# --------------- Python 16 ---------------
+# ---------- Pitagorina teorema ------------
+
+import math
+
+x = float(input("Duzina prve katete je: "))
+y = float(input("Duzina druge katete je: "))
+
+def hipotenuza(x, y):
+    n = x**2
+    m = y**2
+   
+    duzina = math.sqrt(n + m)
+    return duzina
+
+print('\r')
+
+print ("Duzina hipotenuze je: ", (hipotenuza(x, y)),)
