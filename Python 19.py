114c5ea91c4c7a19893fb2bd293c6be585366c55
Borislav Jovanovic <jborislav1970@gmail.com>
2017-11-29 19:10:40 +0100

    Pregled matematickih funkcija - I deo

---
+++ Python 19.py
@@ -0,0 +1,43 @@
+
+# --------------- Python 16 ---------------
+# ---- Pregled Matematickih funkcija I ----
+
+import math
+
+# Vraca veci najblizi celi broj
+print (math.ceil(12.02)) # Rezultat: 13
+print (math.ceil(12.98)) # Rezultat: 13
+print('\r')
+
+# Vraca zaokruzeni najnizi celi broj
+print (math.floor(12.34)) # Rezultat: 12
+print (math.floor(12.98)) # Rezultat: 12
+print('\r')
+
+# Vraca pozitivnu/negativnu vrednost drugog argumenta
+print(math.copysign(12,10)) # Rezultat: 12.0
+print(math.copysign(12,-10)) # Rezultat: -12.0
+print('\r')
+
+# Vraca apsolutnu vrednost broja
+print(math.fabs(12.08)) # Rezultat: 12.08
+print(math.fabs(-12.08)) # Rezultat: -12.08
+print('\r')
+
+# Vraca faktorijal zadatog broja
+print(math.factorial(12)) # Rezultat: 479001600
+print('\r')
+
+# Vraca realni ostatak deljenja dva argumenta
+print(math.fmod(12, 4)) # Rezultat: 0.0
+print(math.fmod(12, 7)) # Rezultat: 5.0
+print('\r')
+
+# Vraca PI
+print (math.pi) # Rezultat: 3.141592653589793
+print('\r')
+
+# Vraca Eulerov broj
+print (math.e) # Rezultat: 2.718281828459045
+
+
