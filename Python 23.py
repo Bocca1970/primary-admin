e2f6157420f4da1adb5d5a1756a4a4c599d6db1b
Borislav Jovanovic <jborislav1970@gmail.com>
2017-11-30 20:41:59 +0100

    Kontrola toka - Nested if

---
+++ Python 23.py
@@ -0,0 +1,18 @@
+
+# --------------- Python 23 ---------------
+# -------- Kontrola toka - Nested if ------
+
+a = 4
+
+# if...else iskaz moze biti unutar drugog if...else iskaza
+if a >= 3:
+    if a == 3: # Uvlacenje teksta (indentation)
+        print("a je jednako 3")
+    else: 
+        print('a je vece od 3')
+else:
+    print("a je manje od 3")
+
+# Nested if se primecuje jedno na osnovu uvlacenja teksta (indentation)
+
+
