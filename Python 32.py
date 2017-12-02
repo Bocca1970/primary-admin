dc09c3787291836ad11b0712ee61ce32de424565
Borislav Jovanovic <jborislav1970@gmail.com>
2017-12-02 22:46:43 +0100

    Kvadrat binoma - zbir kvadrata oba clana i njihovog dvostrukog proizvoda

---
+++ Python 32.py
@@ -0,0 +1,11 @@
+
+# --------------- Python 32 ---------------
+# ------------- Kvadrat binoma ------------
+
+# Kvadrat binoma jednak je zbiru kvadrata oba clana i njihovog dvostrukog proizvoda
+
+a = float(input("Unesi vrednost a: "))
+b = float(input("Unesi vrednost b: "))
+
+pkvadBinoma = (a**2 + 2*a*b + b**2)
+print("Kvadrat binoma je: ", pkvadBinoma)
