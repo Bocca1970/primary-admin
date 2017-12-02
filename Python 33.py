c1c3a9b9cc4cd6c4a1609591d83ec443935d2e7c
Borislav Jovanovic <jborislav1970@gmail.com>
2017-12-02 22:50:13 +0100

    Kvadrat razlike monoma

---
+++ Python 33.py
@@ -0,0 +1,11 @@
+
+# --------------- Python 33 ---------------
+# --------- Kvadrat razlike monoma --------
+
+# Kvadrat razlike monoma jednak je razlici zbira kvadrata clanova monoma i njihovog dvostrukog proizvoda
+
+a = float(input("Unesi vrednost a: "))
+b = float(input("Unesi vrednost b: "))
+
+nkvadBinoma = (a**2 - 2*a*b + b**2)
+print("(a - b) ** 2 = ", nkvadBinoma)
