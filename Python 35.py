73f99b42889641cafb4d1697dcf72aac8fe0311d
Borislav Jovanovic <jborislav1970@gmail.com>
2017-12-02 22:55:40 +0100

    Kubiranje razlike - kub binoma

---
+++ Python 35.py
@@ -0,0 +1,11 @@
+
+# --------------- Python 35 ---------------
+# --------- Kub binoma - razlika ----------
+
+# Kubiranje razlike
+
+a = float(input("Unesi vrednost a: "))
+b = float(input("Unesi vrednost b: "))
+
+nkubBinoma = (a**3 - 3*a**2*b + 3*a*b**2 - b**3)
+print("(a - b) ** 3 = ", nkubBinoma)
