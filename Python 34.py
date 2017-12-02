2e1d31770d77f387fa3018bbc084a2198b2f5f29
Borislav Jovanovic <jborislav1970@gmail.com>
2017-12-02 22:53:56 +0100

    Kubiranje zbira - kub binoma

---
+++ Python 34.py
@@ -0,0 +1,11 @@
+
+# --------------- Python 34 ---------------
+# ---------- Kub Binoma - zbir ------------
+
+# Kubiranje zbira 
+
+a = float(input("Unesi vrednost a: "))
+b = float(input("Unesi vrednost b: "))
+
+pkubBinoma = (a**3 + 3*a**2*b + 3*a*b**2 + b**3)
+print("(a + b) ** 3 = ", pkubBinoma)
