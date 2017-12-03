d9d08a0a89279a30140bed40a3a7a27a7e0a0eec
Borislav Jovanovic <jborislav1970@gmail.com>
2017-12-03 18:29:06 +0100

    Pojam stepenovanja

--- Python 38.py
+++ Python 38.py
@@ -4,8 +4,16 @@
 
 # Matematicka binarna operacija u zapisu x**n
 
+import math
+
 a = int(input("Unesi vrednost a: "))
 b = int(input("Unesi vrednost b: "))
 
-stepen = (a**b)
-print("{0} stepenovano na {1} iznosi: {2}".format(a, b, stepen))
+
+def stepen(a, b):
+    if (a<=0 or b<=0):
+        return 0
+    else:
+        print("{0} stepenovano na {1} iznosi {2}".format(a, b, math.pow(a, b)))
+        
+stepen(a, b)
