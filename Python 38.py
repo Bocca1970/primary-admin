f44878491f6166daecc64bf58008cd633c8816ca
Borislav Jovanovic <jborislav1970@gmail.com>
2017-12-03 17:52:20 +0100

    Matematicka binarna operacija u zapisu x**n

---
+++ Python 38.py
@@ -0,0 +1,11 @@
+
+# --------------- Python 38 ---------------
+# ---------- Pojam Stepenovanja -----------
+
+# Matematicka binarna operacija u zapisu x**n
+
+a = int(input("Unesi vrednost a: "))
+b = int(input("Unesi vrednost b: "))
+
+stepen = (a**b)
+print("{0} stepenovano na {1} iznosi: {2}".format(a, b, stepen))
