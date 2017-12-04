aba084e07f2b4f12a57ef68b3c077dfceb8cb50e
Borislav Jovanovic <jborislav1970@gmail.com>
2017-12-04 17:15:26 +0100

    Izracunavanje povecanja/smanjena cena u %

---
+++ Python 49.py
@@ -0,0 +1,38 @@
+
+# --------------- Python 49 ---------------
+# - Procentualno povecanje/smanjenje cena -
+
+# Povecanje cena u % = 100*(Nova cena-Stara cena)/Stara cena
+# Smanjenje cena u % = 100*(Stara cena-Nova cena)/Stara cena
+
+print("Izaberite jednu od sledecih opcija")
+print('\r')
+
+print("1. Povecanje cena")
+print("2. Smanjenje cena")
+print('\r')
+
+opcija = input("Unesite opciju (1) ili (2): ")
+print('\r')
+
+a = float(input("Stara cena: "))
+b = float(input("Nova cena: "))
+
+
+def procenat(a, b):
+    if (a<=0 or b<=0):
+        return 0
+    else:
+        pass
+
+    povecanje = ((b-a)/a*100)
+    smanjenje = ((a-b)/a*100)
+
+    if opcija == '1':
+        print("Povecanje cene iznosi ", povecanje, '%')
+    elif opcija =='2':
+        print("Smanjenje cene iznosi ", smanjenje, '%')
+    else:
+        print("Pogresan unos!")
+        
+procenat(a, b)
